8083433bb643bed8ff07f18546769d08cb7f5bc8
“CooperBigFoot” <nlazaro@student.ethz.ch>
2024-07-09 16:42:09 +0200

    add docstrings

--- src/gratis_precip/models/mar.py
+++ src/gratis_precip/models/mar.py
@@ -10,25 +10,36 @@ from sklearn.preprocessing import StandardScaler, PowerTransformer
 from statsmodels.tsa.seasonal import STL
 
 from .mar_components import Component
-from ..utils.climate_extreme import ClimateExtreme
-from ..utils.plotting import plot_acf_pacf
+from ..utils.plotting import plot_acf_pacf, plot_time_series, plot_multiple_time_series
 
 logger = logging.getLogger(__name__)
 
 
 class PreprocessStrategy(ABC):
+    """
+    Abstract base class for preprocessing strategies.
+    """
+
     @abstractmethod
     def preprocess(self, data: np.ndarray) -> np.ndarray:
         pass
 
 
 class PostprocessStrategy(ABC):
+    """
+    Abstract base class for postprocessing strategies.
+    """
+
     @abstractmethod
     def postprocess(self, data: np.ndarray) -> np.ndarray:
         pass
 
 
 class DefaultPreprocessStrategy(PreprocessStrategy):
+    """
+    Default preprocessing strategy for the Mixture Autoregressive model.
+    """
+
     def __init__(
         self,
         scaler: StandardScaler,
@@ -43,6 +54,15 @@ class DefaultPreprocessStrategy(PreprocessStrategy):
         self.stl_result = None
 
     def preprocess(self, data: np.ndarray) -> np.ndarray:
+        """
+        Preprocess the input data for the Mixture Autoregressive model.
+
+        Args:
+            data (np.ndarray): The input data to preprocess.
+
+        Returns:
+            np.ndarray: The preprocessed data.
+        """
         logger.info("Preprocessing data...")
         data_adj = np.maximum(data, self.zero_threshold)
         data_transformed = self.power_transformer.fit_transform(
@@ -56,23 +76,34 @@ class DefaultPreprocessStrategy(PreprocessStrategy):
 
 
 class DefaultPostprocessStrategy(PostprocessStrategy):
+    """
+    Default postprocessing strategy for the Mixture Autoregressive model.
+    """
+
     def __init__(
         self,
         scaler: StandardScaler,
         power_transformer: PowerTransformer,
         zero_threshold: float,
-        climate_extreme: ClimateExtreme,
         extreme_threshold: float,
         stl_result: STL,
     ):
         self.scaler = scaler
         self.power_transformer = power_transformer
         self.zero_threshold = zero_threshold
-        self.climate_extreme = climate_extreme
         self.extreme_threshold = extreme_threshold
         self.stl_result = stl_result
 
     def postprocess(self, data: np.ndarray) -> np.ndarray:
+        """
+        Postprocess the generated data for the Mixture Autoregressive model.
+
+        Args:
+            data (np.ndarray): The generated data to postprocess.
+
+        Returns:
+            np.ndarray: The postprocessed data.
+        """
         logger.info("Postprocessing data...")
         unstandardized = self.scaler.inverse_transform(data.reshape(-1, 1)).flatten()
         trend = self.stl_result.trend[: len(unstandardized)]
@@ -82,43 +113,34 @@ class DefaultPostprocessStrategy(PostprocessStrategy):
             reconstructed.reshape(-1, 1)
         ).flatten()
         inv_boxcox_data[inv_boxcox_data < self.zero_threshold] = 0
-
-        threshold = np.percentile(
-            self.climate_extreme.data[self.climate_extreme.data_column_name],
-            self.extreme_threshold * 100,
-        )
-        gev_params = self.climate_extreme.fit_results[
-            self.climate_extreme.data_column_name
-        ]["parameters"]
-
-        extreme_mask = inv_boxcox_data > threshold
-        n_extremes = np.sum(extreme_mask)
-
-        if n_extremes > 0:
-            extreme_values = genextreme.rvs(**gev_params, size=n_extremes)
-            inv_boxcox_data[extreme_mask] = extreme_values
-
-        inv_boxcox_data[inv_boxcox_data < self.zero_threshold] = 0
         return inv_boxcox_data
-
-
-class ClimateExtremeFactory:
-    @staticmethod
-    def create(
-        data: pd.DataFrame, column_name: str, extreme_threshold: float
-    ) -> ClimateExtreme:
-        climate_extreme = ClimateExtreme(data)
-        climate_extreme.fit_genextreme(column_name, extreme_threshold)
-        return climate_extreme
 
 
 @dataclass
 class MARDataGenerator:
+    """
+    Mixture Autoregressive (MAR) data generator.
+
+    This class generates synthetic time series data using a Mixture Autoregressive model.
+
+    Attributes:
+        root_component (Component): The root component of the MAR model.
+        steps (int): The number of steps to generate.
+        seasonal_period (int): The seasonal period of the time series.
+        zero_threshold (float): The threshold for zero values.
+        scaler (StandardScaler): The scaler for the data.
+        power_transformer (PowerTransformer): The power transformer for the data.
+        preprocess_strategy (PreprocessStrategy): The preprocessing strategy.
+        postprocess_strategy (PostprocessStrategy): The postprocessing strategy.
+        original_data (Optional[np.ndarray]): The original time series data.
+        original_index (Optional[pd.DatetimeIndex]): The original time index.
+        data_column_name (str): The name of the data column.
+    """
+
     root_component: Component
     steps: int
     seasonal_period: int = 365
     zero_threshold: float = 0.1
-    extreme_threshold: float = 0.95
     scaler: StandardScaler = field(default_factory=StandardScaler)
     power_transformer: PowerTransformer = field(
         default_factory=lambda: PowerTransformer(method="box-cox")
@@ -127,7 +149,6 @@ class MARDataGenerator:
     postprocess_strategy: PostprocessStrategy = field(init=False)
     original_data: Optional[np.ndarray] = field(init=False, default=None)
     original_index: Optional[pd.DatetimeIndex] = field(init=False, default=None)
-    climate_extreme: Optional[ClimateExtreme] = field(init=False, default=None)
     data_column_name: str = field(init=False, default="data")
 
     def __post_init__(self):
@@ -138,23 +159,16 @@ class MARDataGenerator:
             self.seasonal_period,
         )
 
-    def fit(self, time_series: Union[pd.Series, np.ndarray]) -> None:
-        if isinstance(time_series, pd.Series):
-            self.original_data = time_series.values
-            self.original_index = time_series.index[: self.steps]
-            self.data_column_name = time_series.name if time_series.name else "data"
-        else:
-            self.original_data = time_series
-            self.original_index = pd.date_range(
-                end=pd.Timestamp.today(), periods=len(time_series)
-            )
-            self.data_column_name = "data"
+    def fit(self, time_series: pd.Series) -> None:
+        """
+        Fit the MAR model to the given time series data.
 
-        self.climate_extreme = ClimateExtremeFactory.create(
-            pd.DataFrame({self.data_column_name: self.original_data}),
-            self.data_column_name,
-            self.extreme_threshold,
-        )
+        Args:
+            time_series (pd.Series): The input time series data.
+        """
+        self.original_data = time_series.values
+        self.original_index = time_series.index[: self.steps]
+        self.data_column_name = time_series.name if time_series.name else "data"
 
         preprocessed_data = self.preprocess_strategy.preprocess(self.original_data)
         self.root_component.fit(preprocessed_data)
@@ -163,12 +177,20 @@ class MARDataGenerator:
             self.scaler,
             self.power_transformer,
             self.zero_threshold,
-            self.climate_extreme,
             self.extreme_threshold,
             self.preprocess_strategy.stl_result,
         )
 
     def generate(self, n_trajectories: int) -> pd.DataFrame:
+        """
+        Generate synthetic time series data using the MAR model.
+
+        Args:
+            n_trajectories (int): The number of trajectories to generate.
+
+        Returns:
+            pd.DataFrame: The generated time series data.
+        """
         logger.info(f"Generating {n_trajectories} trajectories...")
         simulations = [
             self._generate_single_trajectory() for _ in range(n_trajectories)
@@ -188,6 +210,13 @@ class MARDataGenerator:
         return df
 
     def _generate_single_trajectory(self) -> np.ndarray:
+        """
+        Generate a single synthetic time series trajectory.
+
+        Returns:
+            np.ndarray: The generated time series data.
+
+        """
         trajectory = np.zeros(self.steps)
         max_order = max(comp.order for comp in self.root_component.components)
         trajectory[:max_order] = np.random.randn(max_order)
@@ -200,14 +229,80 @@ class MARDataGenerator:
         return trajectory
 
     def display_acf_pacf_plots(self, lags: int = 40, alpha: float = 0.05) -> None:
+        """
+        Display the Autocorrelation Function (ACF) and Partial Autocorrelation Function (PACF) plots.
+
+        Args:
+            lags (int, optional): The number of lags to include in the plots. Defaults to 40.
+            alpha (float, optional): The significance level for the confidence intervals. Defaults to 0.05.
+
+        Raises:
+                ValueError: If the original data is not available. Call fit() method first.
+        """
         if self.original_data is None:
             raise ValueError("No data available. Call fit() method first.")
         plot_acf_pacf(self.original_data, lags, alpha)
 
+    def display_acf_pacf_plots(self, lags: int = 40, alpha: float = 0.05) -> None:
+        """
+        Display the Autocorrelation Function (ACF) and Partial Autocorrelation Function (PACF) plots.
+
+        Args:
+            lags (int, optional): The number of lags to include in the plots. Defaults to 40.
+            alpha (float, optional): The significance level for the confidence intervals. Defaults to 0.05.
+        """
+        if self.original_data is None:
+            raise ValueError("No data available. Call fit() method first.")
+        plot_acf_pacf(self.original_data, lags, alpha)
+
+    def plot_original_data(self) -> None:
+        if self.original_data is None:
+            """
+            Display the original time series data.
+            """
+            raise ValueError("No data available. Call fit() method first.")
+        plot_time_series(
+            self.original_data,
+            title="Original Time Series",
+            xlabel="Time",
+            ylabel=self.data_column_name,
+        )
+
+    def plot_generated_trajectories(self, n_trajectories: int = 5) -> None:
+        """
+        Plot the generated time series trajectories.
+
+        Args:
+            n_trajectories (int, optional): The number of trajectories to plot. Defaults to 5.
+        """
+        generated_data = self.generate(n_trajectories)
+        data_dict = {
+            f"Trajectory {i+1}": generated_data[f"Sim_{i+1}"]
+            for i in range(n_trajectories)
+        }
+        plot_multiple_time_series(
+            data_dict,
+            title="Generated Time Series Trajectories",
+            xlabel="Time",
+            ylabel=self.data_column_name,
+        )
+
     @staticmethod
     def save_generated_trajectories(data: pd.DataFrame, file_path: str) -> None:
+        """
+        Save the generated time series data to a CSV file.
+        """
         data.to_csv(file_path)
 
     @staticmethod
     def load_generated_trajectories(file_path: str) -> pd.DataFrame:
+        """
+        Load the generated time series data from a CSV file.
+
+        Args:
+            file_path (str): The file path to load the data from.
+
+        Returns:
+            pd.DataFrame: The loaded time series data.
+        """
         return pd.read_csv(file_path, index_col=0, parse_dates=True)
